4cddcb09a93f223a0dceaf196d461bab556f1371
Yuming Cheung <ymcheung.tw@gmail.com>
2014-06-19 20:15:43 +0800

    copyright & claim

--- apply_page/javascripts/script.js
+++ apply_page/javascripts/script.js
@@ -1,6 +1,25 @@
 /* Author: 
 
+ymcheung
 */
+
+$(document).ready(function() {
+
+$(".layout a").on("mouseenter", function()
+	{
+	$(this).addClass('animated bounce');
+
+	}
+	)
+
+$(".layout a").on("mouseleave", function()
+	{
+	$(this).removeClass('animated bounce');
+
+	}
+	)
+
+});
 
 
 
